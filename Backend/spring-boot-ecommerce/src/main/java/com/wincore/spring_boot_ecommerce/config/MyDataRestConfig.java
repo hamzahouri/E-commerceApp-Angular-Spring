2539cd7a3078bd9d652f529db7e569dee16c81b4
hamzahouri <hamzahouri@gmail.com>
2024-07-08 23:29:43 +0100

    enable post delete from api rest

---
+++ Backend/spring-boot-ecommerce/src/main/java/com/wincore/spring_boot_ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,30 @@
+package com.wincore.spring_boot_ecommerce.config;
+
+import com.wincore.spring_boot_ecommerce.entity.Product;
+import com.wincore.spring_boot_ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        HttpMethod [] unsuportedActions = {HttpMethod.DELETE,HttpMethod.POST,HttpMethod.PUT};
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure( (metadata,httpMethods) -> httpMethods.disable(unsuportedActions))
+                .withCollectionExposure( (metadat,httpMethods) -> httpMethods.disable(unsuportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure( (metadata,httpMethods) -> httpMethods.disable(unsuportedActions))
+                .withCollectionExposure( (metadat,httpMethods) -> httpMethods.disable(unsuportedActions));
+    }
+}
